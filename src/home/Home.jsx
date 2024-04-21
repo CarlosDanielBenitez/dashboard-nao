154ca6638cb0be06300dddc3d183ed4e8055f8d3
Carlos Daniel Benitez <cdanibenz@gmail.com>
2024-04-21 19:27:05 -0300

    [Features] ~~ implement aside navbar in Home component global. Adding Styles.

---
+++ src/home/Home.jsx
@@ -0,0 +1,7 @@
+import NavBar from "../navbar/NavBar";
+
+const Home = () => {
+  return <NavBar />;
+};
+
+export default Home;
---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,62 @@
+import "./navBar.css";
+const NavBar = () => {
+  return (
+    <header className="aside">
+      <img className="aside__off" src="/public/navbar/off.svg" alt="" />
+      <nav className="aside__nav">
+        <ul className="aside__nav__container">
+          <li className="container__box">
+            <img src="/public/navbar/dashboard.svg" alt="" />
+            <h1>Dashboard</h1>
+          </li>
+
+          <li className="container__box">
+            <img src="/public/navbar/order.svg" alt="" />
+            <p>Order</p>
+          </li>
+
+          <li className="container__box">
+            <img src="/public/navbar/products.svg" alt="" />
+            <p>Products</p>
+          </li>
+
+          <li className="container__box">
+            <img src="/public/navbar/shopping.svg" alt="" />
+            <p>Shipping</p>
+          </li>
+
+          <li className="container__box">
+            <img src="/public/navbar/payments.svg" alt="" />
+            <p>Payments</p>
+          </li>
+
+          <li className="container__box">
+            <img src="/public/navbar/settings.svg" alt="" />
+            <p>Settings</p>
+          </li>
+        </ul>
+      </nav>
+      <div className="sopportSection">
+        <p className="customerSupport">Customer Support</p>
+        <textarea
+          placeholder="Ask you query , place
+requests or important
+issues. Our support 
+team will contact 24/7 
+to you. "
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <div className="iconSupport">
+          <img src="/public/navbar/support.svg" alt="" />
+          <p>Connect Now</p>
+        </div>
+        <p className="policy">Terms & Services Privacy Policy</p>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
